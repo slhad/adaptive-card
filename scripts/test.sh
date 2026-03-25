a08cbb0bfa10dcba8abcd54f8b0f3e145395b945
Sylvain Rivierre <sylvainrivierre@gmail.com>
2026-03-25 08:17:05 +0100

    refactor: remove standalone webhook test scripts and related test shell

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Runs the test suite outside of VS Code's debugger auto-attach
-cd "$(dirname "$0")/.."
-unset NODE_OPTIONS
-unset VSCODE_INSPECTOR_OPTIONS
-node --test lib/test/index.test.js
